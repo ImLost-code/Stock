744c39127cc983c2c4e0580f3edeed5384847e09
Mustafa Oğuz <iyimser.tuzlali@hotmail.com>
2019-11-30 06:09:17 +0300

    Products tablosu oluşturuldu.

---
+++ SQL/dbo.Products.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Products]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Brand] VARCHAR(50) NULL, 
+    [Model] VARCHAR(50) NULL, 
+    [Type] INT NOT NULL, 
+    [WorkType] INT NOT NULL, 
+    [Warranty] INT NULL, 
+    [Price] FLOAT NULL, 
+    [CreatedDate] DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    [UpdatedDate] DATETIME NULL
+)
